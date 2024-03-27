5a9f39df18869c6b5f39bd4d805932f296a43f20
harini18122005 <144237501+harini18122005@users.noreply.github.com>
2024-03-27 10:59:32 +0530

    install server.js

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 
 //routes
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/ping', (req, res) => {
+    res.send('pong')
+  });
+
+  app.get('/', (req, res) => {
+    res.send('Hello World to the api!')
   })
+
 
 app.listen(3000,()=>{
     console.log('Node API is running on port 3000 ')
